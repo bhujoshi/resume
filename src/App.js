267ef21b7cad08ead0d56fae0c8c364b1cc6637e
bhuwan <bhuwanchandra.it@gmail.com>
2020-06-01 23:35:57 +0530

    Basic setup added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,140 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="document" style={{ display: 'inline' }}>
+      <div style={{ width: '200px', backgroundColor: '#002e58', color: '#002e58', display: 'inline-block', height: '1540px' }}>
+      </div>
+      <div style={{ width: '500px', marginLeft: '20px', display: 'inline-block' }}>
+
+        <div >
+          <div>
+            <span style={{ color: '#002e58', fontSize: '40px', fontWeight: '600' }}>Bhuwan Joshi</span>
+          </div>
+          <div style={{ color: '#002e58', fontSize: '25px' }}>Software Engineer (Full Stack Developer)</div>
+          <div style={{ marginTop: '16px' }}>
+            <p>
+              <i class="fa fa-map-marker circle-icon"></i>
+              <span></span>
+              <span>New Delhi</span>,
+                        <span>DL</span>,
+                        <span>110093</span>
+            </p>
+            <p>
+              <i class="fa fa-phone circle-icon"></i>
+              <span class="field" id="FIELD_HPHN">705 364 4020</span>
+            </p>
+            <p>
+              <i style={{ padding: '4px 5px' }} class="fa fa-envelope circle-icon"></i>
+              <span>bhuwanchandra.it@gmail.com</span>
+            </p>
+          </div>
+        </div>
+        <div>
+          Forward-thinking Software Engineer (Full-stack-developer) with background working effectively in dynamic
+          environments. Fluent in Javascript programming language. Proud team player focused on achieving project
+          objectives with quality, speed and accuracy.
+            </div>
+        <div>
+          <h2 style={{ color: '#002e58' }}><i style={{ paddingLeft: '10px' }}
+            class="fa fa-puzzle-piece main-circle-icon"></i> Skills </h2>
+          <div>
+            <ul>
+              <li style={{ wordSpacing: '4px' }}>Data Structures &amp; Algorithms </li>
+              <li style={{ wordSpacing: '4px' }}>Javascript, TypeScript</li>
+              <li style={{ wordSpacing: '4px' }}>React, React-native, Redux, MobX</li>
+              <li style={{ wordSpacing: '4px' }}>Unit testing, Jest, Enzyme, Mocha, Chai</li>
+              <li style={{ wordSpacing: '4px' }}>Python, PHP, NodeJS, Mysql</li>
+              <li style={{ wordSpacing: '4px' }}>HTML5 &amp; CSS3, SASS</li>
+              <li style={{ wordSpacing: '4px' }}>Front-end Development</li>
+              <li style={{ wordSpacing: '4px' }}>Git</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <h2 style={{ color: '#002e58' }}> <i style={{ paddingLeft: '9px' }}
+            class="fa fa-briefcase main-circle-icon"></i>Work History </h2>
+          <div style={{ margin: '20px' }}>
+            <div>
+              <div style={{ fontWeight: 600 }}>Software Engineer <span
+                style={{ position: 'absolute', left: '60px', fontSize: '16px', color: '#FFF' }}>2019-04 .
+                                Current</span>
+              </div>
+              <span style={{ fontSize: '16px', fontStyle: 'italic' }}>Edfora infotech Pvt Ltd, Delhi</span>
+              <ul style={{ margin: '5px -22px' }}>
+                <li>Created a Math Html Editor using Javascript</li>
+                <li>Worked on myPat CMS https://authors.mypat.in using (React Redux)</li>
+                <li>Worked on myPat teachers https://teachers.mypat.in using (React, Redux)</li>
+              </ul>
+            </div>
+            <div style={{ paddingTop: '5px' }}>
+              <div style={{ fontWeight: 600 }}>Software Engineer<span
+                style={{ position: 'absolute', left: '60px', fontSize: '16px', color: '#FFF' }}>2018-12 .
+                                2019-03</span>
+              </div>
+              <span style={{ fontSize: '16px', fontStyle: 'italic' }}>Hitachi MGRM Pvt Ltd, Gurugram, HR</span>
+              <ul style={{ margin: '5px -22px' }}>
+                <li>Worked on a HRMS system using (React, Redux)</li>
+              </ul>
+            </div>
+            <div style={{ paddingTop: '5px' }}>
+              <div style={{ fontWeight: 600 }}>Software Engineer<span
+                style={{ position: 'absolute', left: '60px', fontSize: '16px', color: '#FFF' }}>2019-04 .
+                                Current</span>
+              </div>
+              <span style={{ fontSize: '16px', fontStyle: 'italic' }}>Incaendo Technology Pvt Ltd., Delhi,
+                            Delhi</span>
+              <ul style={{ margin: '5px -22px' }}>
+                <li> Worked on https://agriprofocus.com/intro is a social media for farmers using (Javascript,
+                php,
+                                python, MySql) </li>
+              </ul>
+            </div>
+            <div style={{ paddingTop: '5px' }}>
+              <div style={{ fontWeight: 600 }}>PHP Developer<span
+                style={{ position: 'absolute', left: '60px', fontSize: '16px', color: '#FFF' }}>2015-05 .
+                                2016-06</span>
+              </div>
+              <span style={{ fontSize: '16px', fontStyle: 'italic' }}>E-profitbooster Pvt Ltd, Delhi, Delhi</span>
+              <ul style={{ margin: '5px -22px' }}>
+                <li>Created backend for https://www.imperialride.com/ is a shuttle service using (PHP, MySql)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+
+
+        <h2 style={{ color: '#002e58' }}> <i class="fa fa-graduation-cap main-circle-icon"></i> Education</h2>
+        <div style={{ margin: '20px' }}>
+          <div style={{ fontWeight: 600 }}>High School Diploma in Inforamtion Technology <span
+            style={{ position: 'absolute', left: '60px', fontSize: '16px', color: '#FFF' }}>2011-06 . 2014-04</span>
+          </div>
+          <span style={{ fontSize: '16px', fontStyle: 'italic' }}>Govt. polytechnic Nainital, Uttrakhand,
+                        India</span>
+        </div>
+        <div>
+          <h2 style={{ color: '#002e58' }}> <i style={{ paddingLeft: '10px' }} class="fa fa-trophy main-circle-icon"></i>
+                    Accomplishments </h2>
+          <ul style={{ margin: '5px' }}>
+            <li>Created Math Html editor product for my last company</li>
+            <li>Open source contribution on https://github.com/croma-app/croma-react </li>
+            <li>Created a 2048 http://bhujoshi.github.io/ using javascript</li>
+          </ul>
+        </div>
+
+        <div>
+          <h2 style={{ color: '#002e58' }}> <i style={{ paddingLeft: '11px' }} class="fa fa-map-pin main-circle-icon"></i>
+                    Profile Links </h2>
+          <ul style={{ margin: '5px' }}>
+            <li>https://www.linkedin.com/in/bhuwan-joshi-1464a6a3/</li>
+            <li>https://github.com/bhujoshi</li>
+            <li>https://www.codechef.com/users/bhujoshi</li>
+          </ul>
+        </div>
+      </div >
     </div>
   );
 }
